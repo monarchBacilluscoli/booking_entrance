132276769e9f3bf2024c77a9bcc944a1f0b5f4ea
永峰 刘 <20131002682@cug.edu.com>
2018-05-05 14:52:35 +0800

    再次上传

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ function checkSession(sess) {
 
 // 须知页面
 app.get('/', function (req, res) {
+    //res.sendFile(__dirname+'/'+'pages/DongHu_Hotel.html')
     res.sendFile(__dirname + '/' + 'pages/verification.html');
     return;
 })
@@ -122,7 +123,7 @@ app.post('/login_post', urlencodedParser, function (req, res) {
         "pin": req.body.pin,
         "name": req.body.name,
     };
-    connection.query('SELECT * FROM login WHERE pin= ?', [req.body.pin], function (err, result) {   // 使用PIN进行选择只可能有一个返回结果
+    connection.query('SELECT * FROM login WHERE pin= ?', [response.pin], function (err, result) {   // 使用PIN进行选择只可能有一个返回结果
         if (err) {
             // 纯粹错误
             console.log('[SELECT ERROR] - ', err.message);
@@ -135,7 +136,7 @@ app.post('/login_post', urlencodedParser, function (req, res) {
             if (result[0] == undefined) {
                 // PIN不存在
                 console.log('------------------------hold(PIN)--------------------------');
-                console.log("WRONG PIN: " + req.body.pin);
+                console.log("WRONG PIN: " + response.pin);
                 console.log("Hold time: " + new Date(Date.now()));  // 输出时间
                 console.log("Clien info: " + JSON.stringify(req.headers))
                 console.log("IP: " + getClientIp(req));
@@ -147,7 +148,7 @@ app.post('/login_post', urlencodedParser, function (req, res) {
                 // 密码错误
                 console.log('------------------------hold(NAME)--------------------------');
                 console.log("PIN: " + result[0].pin);
-                console.log("wrong name: " + req.body.username);
+                console.log("wrong name: " + response.username);
                 console.log("Hold time: " + new Date(Date.now()));  // 输出时间
                 console.log("Clien info: " + JSON.stringify(req.headers));
                 console.log("IP: " + getClientIp(req));
@@ -171,8 +172,8 @@ app.post('/login_post', urlencodedParser, function (req, res) {
                         return;
                     }
                     else {
-                        req.session.loginUser = req.body.pin;
-                        req.session.loginName = req.body.name;// 登陆成功，设置session
+                        req.session.loginUser = response.pin;
+                        req.session.loginName = response.name;// 登陆成功，设置session
                         //console.log(req.session.loginName);
                         res.sendFile(__dirname + '/pages/choice.html');
                         return;
@@ -204,26 +205,25 @@ app.get('/donghu_get', function (req, res) {
     } else {
         // 获取数据：先获取房间信息
         connection.query('SELECT * FROM donghu WHERE sn=1', function (err_r, result_r) {
-            if (err) {
+            if (err_r) {
                 // 错误处理逻辑（房间应该不会出错）
                 return;
             } else {
                 // 获取数据：再获取个人信息
                 connection.query('SELECT * FROM login WHERE pin = ?', [req.session.loginUser], function (err_u, result_u) {
-                    if (err) {
+                    if (err_u) {
                         // 错误处理逻辑（既然注册过那么应该也不会出错）
                         return;
                     } else {
                         // 渲染网页并显示
-                        res.render('verification', { 
+                        res.render('donghu', {
                             // 用户信息
-                            name: result_u[0].username,                         phone: result_u[0].phone, email:result_u[0].email,
+                            name: result_u[0].username, phone: result_u[0].phone, email: result_u[0].email,
                             // 房间信息
-                            tingtao2_biao: result_r[0].tingtao2_biao_current,   tingtao2_dan:result_r[0].tingtao2_dan_current, 
-                            nanshanyi_biao:result_r[0].nanshanyi_biao_current,  nanshanyi_dan:result_r[0].nanshanyi_dan_current,
-                            baihua2_biao:result_r[0].baihua2_biao_current
+                            tingtao2_biao: result_r[0].tingtao2_biao_current, tingtao2_dan: result_r[0].tingtao2_dan_current,
+                            nanshanyi_biao: result_r[0].nanshanyi_biao_current, nanshanyi_dan: result_r[0].nanshanyi_dan_current,
+                            baihua2_biao: result_r[0].baihua2_biao_current
                         });
-                        return;
                     }
                 })
             }
@@ -238,40 +238,84 @@ app.post('/donghu_reservation_post', urlencodedParser, function (req, res) {
         res.render('verification', { s: warningString })
         return;
     } else {
+        var response = {
+            "pin": req.session.loginUser,
+            "name":req.body.name,
+            "phone":req.body.phonenum,
+            "email":req.body.email,
+            "TTB": req.body.input-num_TTB,
+            "TTD":req.body.input-num_TTD,
+            "NSB":req.body.input-num_NSB,
+            "NSD":req.body.input-num_NSD,
+            "BHB":req.body.input-num_BHB,
+            "CIN":req.body.startDate,
+            "COUT":req.body.endDate,
+            // "DPT":
+        };
         // TODO: 执行逻辑: 数据写入数据库，回复邮件，页面提醒
-        // 添加订单
+        // 先处理剩余房间数，如果不够则停止进一步处理
+        connection.query('UPDATE donghu SET tingtao2_biao_current=tingtao2_biao-?, tingtao2_dan_current-?',[req.body.input-num_TTB,req.body.input-num_TTD],function(err_r,result_r){
+            if(err){
+                console.log(err_r); // TODO：这里有一个减法超过无符号整形范围的错误，利用一下就不自己写代码了。
+                res.send('Room number not valid.');
+                return;
+            }
+            else{
+                console.log(result_r);
+                // 再处理订单
+                var order = 'INSERT donghu VALUES(pin, tingtao_2_biao,tingtao2_dan,nanshanyi_biao,nanshanyi_dan,baihua2_biao,checkin,checkout)';
+                connection.query(order,[response.pin,response.TTB,response.TTD,response.NSB,response.NSD,response.BHB,response.CIN,response.COUT],function(err_o,result_o){
+                    if(err_o){
+                        console.log(err_o);
+                        return;
+                    }
+                    else{
+                        // 再处理个人信息
+                        var userInfo = 'UPDATE login SET phone=?, email=? WHERE pin= ?';
+                        connection.query(userInfo,[response.phone,response.email,response.pin],function(err_u,result_u){
+                            if(err){
+                                consol.log(err);
+                            }
+                            else{
+                                
+                            }
+                        });
+                    }
+                });
+                return;
+            }
+        });
         // connection.query('INSERT orders (pin, chekin, checkout, tingtao2_biao,tingtao2_dan,nanshanyi_biao,nanshanyi_dan,baihua2_biao, deposit) values(?,?,?,?,?,?,?,?,?)',[,,,,,,,,])
     }
-
 })
 
-// TODO: 东湖订房表单请求处理, 此处需要session
-app.post('/donghu_book_post', function (req, res) {
-    // TODO: 数据
-    // 订单数据
-    var order_res = {
-        "pin": req.query.pin,
-        "tingtao2_biao": req.query.tingtao2_biao,
-        "tingtao2_dan": req.query.tingtao2_dan,
-        "nanshanyi_biao": req.query.nanshanyi_biao,
-        "nanshanyi_dan": req.query.nanshanyi_dan,
-        "baihua2_biao": req.query.baihua2_biao,
-        "baihua2_dan": req.query.baihua2_dan
-    };
-    var user_res={
-        "pin": req.query.pin,
-        "name": req.query.name,
-        "phone": req.query.phone,
-        "email": req.query.email
-    };
-    // TODO: 修改数据库数据
-    // TODO: 首先按照房间（判断可以前台写，但不知道是否可行或者安全）
-    connection.query('UPDATE room SET tingtao2_biao_current=tingtao2_biao_current-?,tingtao2_dan_current=tingtao2_dan_current-?,nanshanyi_biao_current=nanshanyi_biao_current-?,nanshanyi_dan_current=nanshanyi_dan_current- WHRER sn=1', [req.query.tingtao2_biao,req.query.tingtao2_dan,req.query.nanshanyi_biao,req.query.nanshanyi_dan,req.query.baihua2_biao], function (err, result) {
+// // TODO: 东湖订房表单请求处理, 此处需要session
+// app.post('/donghu_book_post', function (req, res) {
+//     // TODO: 数据
+//     // 订单数据
+//     var order_res = {
+//         "pin": req.query.pin,
+//         "tingtao2_biao": req.query.tingtao2_biao,
+//         "tingtao2_dan": req.query.tingtao2_dan,
+//         "nanshanyi_biao": req.query.nanshanyi_biao,
+//         "nanshanyi_dan": req.query.nanshanyi_dan,
+//         "baihua2_biao": req.query.baihua2_biao,
+//         "baihua2_dan": req.query.baihua2_dan
+//     };
+//     var user_res = {
+//         "pin": req.query.pin,
+//         "name": req.query.name,
+//         "phone": req.query.phone,
+//         "email": req.query.email
+//     };
+//     // TODO: 修改数据库数据
+//     // TODO: 首先按照房间（判断可以前台写，但不知道是否可行或者安全）
+//     connection.query('UPDATE room SET tingtao2_biao_current=tingtao2_biao_current-?,tingtao2_dan_current=tingtao2_dan_current-?,nanshanyi_biao_current=nanshanyi_biao_current-?,nanshanyi_dan_current=nanshanyi_dan_current- WHRER sn=1', [req.query.tingtao2_biao, req.query.tingtao2_dan, req.query.nanshanyi_biao, req.query.nanshanyi_dan, req.query.baihua2_biao], function (err, result) {
 
-    })
-    // TODO: 按照用户
-    //connection.query(,,)
-})
+//     })
+//     // TODO: 按照用户
+//     //connection.query(,,)
+// })
 
 // TODO: 管理员修改数据请求，此处出于安全考虑需要session
 app.post('/menager_post', function (req, res) {
@@ -288,7 +332,6 @@ app.get('/logout', function (req, res, next) {
         }
     })
 })
-
 
 // 设置监听端口为8081
 var server = app.listen(8081, function () {
